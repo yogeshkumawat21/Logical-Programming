e22dafb4929a0e36cddf89853726ccbe2eb1b52b
Yogesh kumawat <114004390+yogeshkumawat21@users.noreply.github.com>
2024-07-05 23:46:41 +0530

    CheckAsciiValue.java

---
+++ CheckAsciiValue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main 
+{
+    public static void main (String[] args) {
+       
+       int b =67;
+       char a = (char)b;
+       System.out.print(a);
+    }
+}
+
+
+//char to ascii
+import java.util.*;
+public class Main 
+{
+    public static void main (String[] args) {
+       char a = 'b';
+       int b =a;
+       System.out.print(b);
+    }
+}
+
