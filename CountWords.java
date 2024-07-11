5cd467384b6da89803cf28b7888c1065c06a502f
Yogesh kumawat <114004390+yogeshkumawat21@users.noreply.github.com>
2024-07-11 23:20:31 +0530

    CountWords.java

---
+++ CountWords.java
@@ -0,0 +1,8 @@
+public class Main 
+{
+    public static void main(String[] args) {
+       String a = "hello my name is yogesh kumawat i am student of java ";
+       String b[]=a.split(" ");
+       System.out.println(b.length);
+    }
+}
